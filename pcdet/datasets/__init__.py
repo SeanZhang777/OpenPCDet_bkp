cafd772ebcf6a9884eb6fab2a58a134560ed34de
Sean Zhang <sean_zhang777@163.com>
2021-01-08 15:04:49 +0800

    add maxus+robosense hybrid dataset

--- pcdet/datasets/__init__.py
+++ pcdet/datasets/__init__.py
@@ -8,12 +8,14 @@ from .dataset import DatasetTemplate
 from .kitti.kitti_dataset import KittiDataset
 from .nuscenes.nuscenes_dataset import NuScenesDataset
 from .waymo.waymo_dataset import WaymoDataset
+from .maxus.maxus_dataset import MaxusDataset
 
 __all__ = {
     'DatasetTemplate': DatasetTemplate,
     'KittiDataset': KittiDataset,
     'NuScenesDataset': NuScenesDataset,
-    'WaymoDataset': WaymoDataset
+    'WaymoDataset': WaymoDataset,
+    'MaxusDataset': MaxusDataset,
 }
 
 
---
+++ pcdet/datasets/maxus/maxus_dataset.py
@@ -0,0 +1,382 @@
+import copy
+import pickle
+
+import numpy as np
+from skimage import io
+
+from ...ops.roiaware_pool3d import roiaware_pool3d_utils
+from ...utils import box_utils, calibration_kitti, common_utils, object3d_kitti
+from ..dataset import DatasetTemplate
+
+point_num_threshold = {
+    'Car': 15,
+    'Large_vehicle': 20,
+    'Pedestrian': 5,
+    'Cyclist': 5,
+    'Others': 0,
+}
+
+class MaxusDataset(DatasetTemplate):
+    def __init__(self, dataset_cfg, class_names, training=True, root_path=None, logger=None):
+        """
+        Args:
+            root_path:
+            dataset_cfg:
+            class_names:
+            training:
+            logger:
+        """
+        super().__init__(
+            dataset_cfg=dataset_cfg, class_names=class_names, training=training, root_path=root_path, logger=logger
+        )
+        self.split = self.dataset_cfg.DATA_SPLIT[self.mode]
+        self.root_split_path = self.root_path / ('training' if self.split != 'test' else 'testing')
+
+        split_dir = self.root_path / 'ImageSets' / (self.split + '.txt')
+        self.sample_id_list = [x.strip() for x in open(split_dir).readlines()] if split_dir.exists() else None
+
+        self.maxus_infos = []
+        self.include_maxus_data(self.mode)
+
+    def include_maxus_data(self, mode):
+        if self.logger is not None:
+            self.logger.info('Loading MAXUS dataset')
+        maxus_infos = []
+
+        for info_path in self.dataset_cfg.INFO_PATH[mode]:
+            info_path = self.root_path / info_path
+            if not info_path.exists():
+                continue
+            with open(info_path, 'rb') as f:
+                infos = pickle.load(f)
+                maxus_infos.extend(infos)
+
+        self.maxus_infos.extend(maxus_infos)
+
+        if self.logger is not None:
+            self.logger.info('Total samples for MAXUS dataset: %d' % (len(maxus_infos)))
+
+    def set_split(self, split):
+        super().__init__(
+            dataset_cfg=self.dataset_cfg, class_names=self.class_names, training=self.training, root_path=self.root_path, logger=self.logger
+        )
+        self.split = split
+        self.root_split_path = self.root_path / ('training' if self.split != 'test' else 'testing')
+
+        split_dir = self.root_path / 'ImageSets' / (self.split + '.txt')
+        self.sample_id_list = [x.strip() for x in open(split_dir).readlines()] if split_dir.exists() else None
+
+    def get_lidar(self, idx):
+        lidar_file = self.root_split_path / 'pointcloud' / ('%s.bin' % idx)
+        assert lidar_file.exists()
+        return np.fromfile(str(lidar_file), dtype=np.float32).reshape(-1, 4)
+
+    def get_image_shape(self, idx):
+        img_file = self.root_split_path / 'image_2' / ('%s.png' % idx)
+        assert img_file.exists()
+        return np.array(io.imread(img_file).shape[:2], dtype=np.int32)
+
+    def get_label(self, idx):
+        label_file = self.root_split_path / 'label' / ('%s.txt' % idx)
+        assert label_file.exists()
+        return np.genfromtxt(label_file).reshape(-1, 10)
+
+    def get_calib(self, idx):
+        calib_file = self.root_split_path / 'calib' / ('%s.txt' % idx)
+        assert calib_file.exists()
+        return calibration_kitti.Calibration(calib_file)
+
+    def get_road_plane(self, idx):
+        plane_file = self.root_split_path / 'planes' / ('%s.txt' % idx)
+        if not plane_file.exists():
+            return None
+
+        with open(plane_file, 'r') as f:
+            lines = f.readlines()
+        lines = [float(i) for i in lines[3].split()]
+        plane = np.asarray(lines)
+
+        # Ensure normal is always facing up, this is in the rectified camera coordinate
+        if plane[1] > 0:
+            plane = -plane
+
+        norm = np.linalg.norm(plane[0:3])
+        plane = plane / norm
+        return plane
+
+    @staticmethod
+    def get_fov_flag(pts_rect, img_shape, calib):
+        """
+        Args:
+            pts_rect:
+            img_shape:
+            calib:
+
+        Returns:
+
+        """
+        pts_img, pts_rect_depth = calib.rect_to_img(pts_rect)
+        val_flag_1 = np.logical_and(pts_img[:, 0] >= 0, pts_img[:, 0] < img_shape[1])
+        val_flag_2 = np.logical_and(pts_img[:, 1] >= 0, pts_img[:, 1] < img_shape[0])
+        val_flag_merge = np.logical_and(val_flag_1, val_flag_2)
+        pts_valid_flag = np.logical_and(val_flag_merge, pts_rect_depth >= 0)
+
+        return pts_valid_flag
+
+    def get_infos(self, num_workers=4, has_label=True, count_inside_pts=True, sample_id_list=None):
+        import concurrent.futures as futures
+
+        def process_single_scene(sample_idx):
+            print('%s sample_idx: %s' % (self.split, sample_idx))
+            info = {}
+            pc_info = {'num_features': 4, 'lidar_idx': sample_idx}
+            info['point_cloud'] = pc_info
+
+            if has_label:
+                name_mapping = {
+                    0: 'Car',
+                    1: 'Large_vehicle',
+                    2: 'Pedestrian',
+                    3: 'Cyclist',
+                    4: 'Others'
+                }
+                labels = self.get_label(sample_idx)
+                annotations = {}
+                annotations['name'] = np.array([name_mapping[int(id)] for id in labels[:, 0]])
+
+                num_objects = len([name for name in annotations['name'] if name != 'Others'])
+                num_gt = len(annotations['name'])
+                index = list(range(num_objects)) + [-1] * (num_gt - num_objects)
+                annotations['index'] = np.array(index, dtype=np.int32)
+                gt_boxes_lidar = labels[:, 1:8]
+                annotations['gt_boxes_lidar'] = gt_boxes_lidar
+                annotations['trunct'] = labels[:, 8].astype(np.int32)
+                annotations['in_driving_area'] = labels[:, 9].astype(np.int32)
+                annotations['difficulty'] = np.zeros(num_gt, dtype=np.int32)
+                info['annos'] = annotations
+
+                if count_inside_pts:
+                    points = self.get_lidar(sample_idx)
+
+                    corners_lidar = box_utils.boxes_to_corners_3d(gt_boxes_lidar)
+                    num_points_in_gt = -np.ones(num_gt, dtype=np.int32)
+
+                    for k in range(num_objects):
+                        flag = box_utils.in_hull(points[:, 0:3], corners_lidar[k])
+                        num_points_in_gt[k] = flag.sum()
+                    annotations['num_points_in_gt'] = num_points_in_gt
+
+            return info
+
+        sample_id_list = sample_id_list if sample_id_list is not None else self.sample_id_list
+        with futures.ThreadPoolExecutor(num_workers) as executor:
+            infos = executor.map(process_single_scene, sample_id_list)
+        return list(infos)
+
+    def create_groundtruth_database(self, info_path=None, used_classes=None, split='train'):
+        import torch
+
+        database_save_path = Path(self.root_path) / ('gt_database' if split == 'train' else ('gt_database_%s' % split))
+        db_info_save_path = Path(self.root_path) / ('maxus_dbinfos_%s.pkl' % split)
+
+        database_save_path.mkdir(parents=True, exist_ok=True)
+        all_db_infos = {}
+
+        with open(info_path, 'rb') as f:
+            infos = pickle.load(f)
+
+        for k in range(len(infos)):
+            print('gt_database sample: %d/%d' % (k + 1, len(infos)))
+            info = infos[k]
+            sample_idx = info['point_cloud']['lidar_idx']
+            points = self.get_lidar(sample_idx)
+            annos = info['annos']
+            names = annos['name']
+            difficulty = annos['difficulty']
+            gt_boxes = annos['gt_boxes_lidar']
+            num_points_in_gt = annos['num_points_in_gt']
+            trunct = annos['trunct']
+
+            num_obj = gt_boxes.shape[0]
+            point_indices = roiaware_pool3d_utils.points_in_boxes_cpu(
+                torch.from_numpy(points[:, 0:3]), torch.from_numpy(gt_boxes)
+            ).numpy()  # (nboxes, npoints)
+
+            for i in range(num_obj):
+                if trunct[i] != 2 or num_points_in_gt[i] < point_num_threshold[names[i]]:
+                    continue
+                filename = '%s_%s_%d.bin' % (sample_idx, names[i], i)
+                filepath = database_save_path / filename
+                gt_points = points[point_indices[i] > 0]
+
+                gt_points[:, :3] -= gt_boxes[i, :3]
+                with open(filepath, 'w') as f:
+                    gt_points.tofile(f)
+
+                if (used_classes is None) or names[i] in used_classes:
+                    db_path = str(filepath.relative_to(self.root_path))  # gt_database/xxxxx.bin
+                    db_info = {'name': names[i], 'path': db_path, 'image_idx': sample_idx, 'gt_idx': i,
+                               'box3d_lidar': gt_boxes[i], 'num_points_in_gt': gt_points.shape[0],
+                               'difficulty': difficulty[i]}
+                    if names[i] in all_db_infos:
+                        all_db_infos[names[i]].append(db_info)
+                    else:
+                        all_db_infos[names[i]] = [db_info]
+        for k, v in all_db_infos.items():
+            print('Database %s: %d' % (k, len(v)))
+
+        with open(db_info_save_path, 'wb') as f:
+            pickle.dump(all_db_infos, f)
+
+    @staticmethod
+    def generate_prediction_dicts(batch_dict, pred_dicts, class_names, output_path=None):
+        """
+        Args:
+            batch_dict:
+                frame_id:
+            pred_dicts: list of pred_dicts
+                pred_boxes: (N, 7), Tensor
+                pred_scores: (N), Tensor
+                pred_labels: (N), Tensor
+            class_names:
+            output_path:
+
+        Returns:
+
+        """
+        def get_template_prediction(num_samples):
+            ret_dict = {
+                'name': np.zeros(num_samples),
+                'score': np.zeros(num_samples),
+                'gt_boxes_lidar': np.zeros([num_samples, 7])
+            }
+            return ret_dict
+
+        def generate_single_sample_dict(batch_index, box_dict):
+            pred_scores = box_dict['pred_scores'].cpu().numpy()
+            pred_boxes = box_dict['pred_boxes'].cpu().numpy()
+            pred_labels = box_dict['pred_labels'].cpu().numpy()
+            pred_dict = get_template_prediction(pred_scores.shape[0])
+            if pred_scores.shape[0] == 0:
+                return pred_dict
+
+            pred_dict['name'] = np.array(class_names)[pred_labels - 1]
+            pred_dict['score'] = pred_scores
+            pred_dict['gt_boxes_lidar'] = pred_boxes
+
+            return pred_dict
+
+        annos = []
+        for index, box_dict in enumerate(pred_dicts):
+            frame_id = batch_dict['frame_id'][index]
+
+            single_pred_dict = generate_single_sample_dict(index, box_dict)
+            single_pred_dict['frame_id'] = frame_id
+            annos.append(single_pred_dict)
+
+        return annos
+
+    def evaluation(self, det_annos, class_names, **kwargs):
+        if 'annos' not in self.maxus_infos[0].keys():
+            return None, {}
+
+        from .maxus_object_eval_python import eval as maxus_eval
+
+        eval_det_annos = copy.deepcopy(det_annos)
+        eval_gt_annos = [copy.deepcopy(info['annos']) for info in self.maxus_infos]
+        # eval_det_annos = []
+        # for gt in eval_gt_annos:
+        #     dt_anno = gt.copy()
+        #     dt_anno["score"] = np.ones(gt["gt_boxes_lidar"].shape[0])
+        #     eval_det_annos.append(dt_anno)
+        ap_result_str, ap_dict = maxus_eval.get_mAP_eval_result(eval_gt_annos, eval_det_annos, class_names)
+
+        return ap_result_str, ap_dict
+
+    def __len__(self):
+        if self._merge_all_iters_to_one_epoch:
+            return len(self.maxus_infos) * self.total_epochs
+
+        return len(self.maxus_infos)
+
+    def __getitem__(self, index):
+        # index = 4
+        if self._merge_all_iters_to_one_epoch:
+            index = index % len(self.maxus_infos)
+
+        info = copy.deepcopy(self.maxus_infos[index])
+
+        sample_idx = info['point_cloud']['lidar_idx']
+
+        points = self.get_lidar(sample_idx)
+
+        input_dict = {
+            'points': points,
+            'frame_id': sample_idx,
+        }
+
+        if 'annos' in info:
+            annos = info['annos']
+            annos = common_utils.drop_info_with_name(annos, name='Others')
+            #annos = common_utils.drop_info_with_point_num(annos, point_th=point_num_threshold)
+            gt_names = annos['name']
+            gt_boxes_lidar = annos['gt_boxes_lidar']
+
+            input_dict.update({
+                'gt_names': gt_names,
+                'gt_boxes': gt_boxes_lidar
+            })
+
+        data_dict = self.prepare_data(data_dict=input_dict)
+
+        return data_dict
+
+
+def create_maxus_infos(dataset_cfg, class_names, data_path, save_path, workers=4):
+    dataset = MaxusDataset(dataset_cfg=dataset_cfg, class_names=class_names, root_path=data_path, training=False)
+    train_split, val_split = 'train', 'val'
+
+    train_filename = save_path / ('maxus_infos_%s.pkl' % train_split)
+    val_filename = save_path / ('maxus_infos_%s.pkl' % val_split)
+    trainval_filename = save_path / 'maxus_infos_trainval.pkl'
+    test_filename = save_path / 'maxus_infos_test.pkl'
+
+    print('---------------Start to generate data infos---------------')
+
+    dataset.set_split(train_split)
+    maxus_infos_train = dataset.get_infos(num_workers=workers, has_label=True, count_inside_pts=True)
+    with open(train_filename, 'wb') as f:
+        pickle.dump(maxus_infos_train, f)
+    print('Maxus info train file is saved to %s' % train_filename)
+
+    dataset.set_split(val_split)
+    maxus_infos_val = dataset.get_infos(num_workers=workers, has_label=True, count_inside_pts=True)
+    with open(val_filename, 'wb') as f:
+        pickle.dump(maxus_infos_val, f)
+    print('Maxus info val file is saved to %s' % val_filename)
+
+    with open(trainval_filename, 'wb') as f:
+        pickle.dump(maxus_infos_train + maxus_infos_val, f)
+    print('Maxus info trainval file is saved to %s' % trainval_filename)
+
+    print('---------------Start create groundtruth database for data augmentation---------------')
+    dataset.set_split(train_split)
+    dataset.create_groundtruth_database(train_filename, split=train_split)
+
+    print('---------------Data preparation Done---------------')
+
+
+if __name__ == '__main__':
+    import sys
+    if sys.argv.__len__() > 1 and sys.argv[1] == 'create_maxus_infos':
+        import yaml
+        from pathlib import Path
+        from easydict import EasyDict
+        dataset_cfg = EasyDict(yaml.load(open(sys.argv[2])))
+        ROOT_DIR = (Path(__file__).resolve().parent / '../../../').resolve()
+        create_maxus_infos(
+            dataset_cfg=dataset_cfg,
+            class_names=['Car', 'Large_vehicle', 'Pedestrian', 'Cyclist'],
+            data_path=ROOT_DIR / 'data' / 'maxus',
+            save_path=ROOT_DIR / 'data' / 'maxus'
+        )
