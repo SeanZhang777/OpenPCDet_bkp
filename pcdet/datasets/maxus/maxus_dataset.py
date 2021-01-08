6f8338aa08c191ecd61ccfba2ad17ce889652ab2
Sean Zhang <sean_zhang777@163.com>
2021-01-08 17:34:13 +0800

    add CBGS support to maxus dataset

--- pcdet/datasets/maxus/maxus_dataset.py
+++ pcdet/datasets/maxus/maxus_dataset.py
@@ -37,6 +37,8 @@ class MaxusDataset(DatasetTemplate):
 
         self.maxus_infos = []
         self.include_maxus_data(self.mode)
+        if self.training and self.dataset_cfg.get('BALANCED_RESAMPLING', False):
+            self.maxus_infos = self.balanced_infos_resampling(self.maxus_infos)
 
     def include_maxus_data(self, mode):
         if self.logger is not None:
@@ -55,6 +57,43 @@ class MaxusDataset(DatasetTemplate):
 
         if self.logger is not None:
             self.logger.info('Total samples for MAXUS dataset: %d' % (len(maxus_infos)))
+
+    def balanced_infos_resampling(self, infos):
+        """
+        Class-balanced sampling of nuScenes dataset from https://arxiv.org/abs/1908.09492
+        """
+        if self.class_names is None:
+            return infos
+
+        cls_infos = {name: [] for name in self.class_names}
+        for info in infos:
+            for name in set(info['annos']['name']):
+                if name in self.class_names:
+                    cls_infos[name].append(info)
+
+        duplicated_samples = sum([len(v) for _, v in cls_infos.items()])
+        cls_dist = {k: len(v) / duplicated_samples for k, v in cls_infos.items()}
+
+        sampled_infos = []
+
+        frac = 1.0 / len(self.class_names)
+        ratios = [frac / v for v in cls_dist.values()]
+
+        for cur_cls_infos, ratio in zip(list(cls_infos.values()), ratios):
+            sampled_infos += np.random.choice(
+                cur_cls_infos, int(len(cur_cls_infos) * ratio)
+            ).tolist()
+        self.logger.info('Total samples after balanced resampling: %s' % (len(sampled_infos)))
+
+        cls_infos_new = {name: [] for name in self.class_names}
+        for info in sampled_infos:
+            for name in set(info['annos']['name']):
+                if name in self.class_names:
+                    cls_infos_new[name].append(info)
+
+        cls_dist_new = {k: len(v) / len(sampled_infos) for k, v in cls_infos_new.items()}
+
+        return sampled_infos
 
     def set_split(self, split):
         super().__init__(
